90ca4507b57c79ae79f67cf7e55cbaa588d7a3af
khmob <99550420+khmob@users.noreply.github.com>
2025-11-18 10:21:34 +0200

    feat(M-12329): add vue-i18n eslint config (#47)
    * feat(M-12329): create vue-i18n config
    
    * feat(M-12329): bump and tag version

---
+++ src/configs/rules/mobal-vue-i18n-rules.js
@@ -0,0 +1,10 @@
+export default {
+    '@intlify/vue-i18n/no-duplicate-keys-in-locale': 'error',
+    '@intlify/vue-i18n/no-missing-keys-in-other-locales': 'error',
+    '@intlify/vue-i18n/no-unused-keys': 'error',
+    '@intlify/vue-i18n/prefer-linked-key-with-paren': 'error',
+    '@intlify/vue-i18n/key-format-style': [
+        'error',
+        'camelCase',
+    ],
+}
